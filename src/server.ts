0487d52ec700a99650c8c438ed1801d47edbfed2
Nidhi <nidhi@auberginesolutions.com>
2023-06-05 15:00:29 +0530

    added | creating cluster for the node js application

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,35 @@
-import express from 'express';
-const app = express();
+import express, { Express } from 'express';
+import cluster from 'cluster';
+import * as http from 'http';
+import { cpus } from 'os';
+import process from 'process';
+
+
+const app: Express = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+//getting the number of cores
+const numCPUs = cpus().length;
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-})
+// creating clusters
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  // workers can share any TCP connection
+  // in this case it is an HTTP server
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+  }).listen(port);
+
+  console.log(`Worker ${process.pid} started`);
+}
