6567e55fc310b8b7a88be35cbe153710cd7c5809
Nidhi <nidhi@auberginesolutions.com>
2023-06-19 13:25:11 +0530

    fixed | concurrent ticket booking issue

--- src/Controller/Tickets.controller.ts
+++ src/Controller/Tickets.controller.ts
@@ -6,22 +6,27 @@ import { Ride } from '../Models/Rides';
 export const bookTicket: RequestHandler = async (req, res, next) => {
 	const { ticket } = req.body;
 	const newTicket = new Ticket(ticket);
-	const rideDetails = await Ride.findById({ _id: newTicket.rideId })
 
-	if (rideDetails) {
-		const ticketCountForTheDate: Number = await Ticket.count({ rideId: newTicket.rideId, date: newTicket.date });
-		if (rideDetails.seatsPerDay > ticketCountForTheDate) {
-			newTicket.save().then((createdTicket) => {
-				res.status(200).json({ code: "success", ticket: createdTicket })
-			}).catch((error) => {
-				console.log("Error: ");
-				console.log({ error });
-				res.status(500).json({ code: "error", message: "Error creating ticket." })
-			})
-		} else {
-			res.status(409).json({ code: "error", message: "Ride is fully booked" })
-		}
-	} else {
-		res.status(409).json({ code: "error", message: "Ride not found." })
-	}
+	Ride.findOneAndUpdate({ _id: newTicket.rideId }, { $inc: { availableSeats: -1 } }, { new: true, runValidators: true, })
+		.then(async (updatedRideSeats) => {
+			
+			if(!updatedRideSeats){
+				console.log("Ride not available");
+				return res.status(409).json({ code: "error", message: "Ride not available." })
+			}
+
+			if (Number(updatedRideSeats.availableSeats) >= 0) {
+				newTicket.save().then((createdTicket) => {
+					res.status(200).json({ code: "success", ticket: createdTicket })
+				}).catch(async (error) => {
+					console.log("Error creating ticket");
+					await Ride.updateOne({ _id: newTicket.rideId }, { $inc: { availableSeats: 1 } })
+					res.status(500).json({ code: "error", message: error.message })
+				})
+			} else {
+				console.log("Seats not available");
+				await Ride.updateOne({ _id: newTicket.rideId }, { $inc: { availableSeats: 1 } })
+				res.status(409).json({ code: "error", message: "Seats not available." })
+			}
+		})
 };
